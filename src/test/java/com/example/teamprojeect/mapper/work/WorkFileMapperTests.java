8043f629bca58841e7567fb233e374bc9b06cab2
wanggoNya <tndus6384@naver.com>
2022-06-17 05:47:15 +0900

    Buskers 프로젝트 로그인, 마이페이지 최종 프론트 작업물

--- src/test/java/com/example/teamprojeect/mapper/work/WorkFileMapperTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.teamprojeect.mapper.work;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@Slf4j
-public class WorkFileMapperTests {
-}
